272f16934b043b2b128d3b0c4a5ff81b5765226b
tiagorocha1 <tiagoferreira.info@gmail.com>
2017-02-25 18:29:32 -0300

    Refatoração

--- introducao-javascript/js/filtra.js
+++ introducao-javascript/js/filtra.js
@@ -8,7 +8,7 @@ campoFiltro.addEventListener("input", () => {
         pacientes.array.forEach((paciente) => {
           var tdNome = paciente.querySelector(".info-nome");
           var nome = tdNome.textContent;
-          var expressao = new RegExp(this.value, "i");
+          var expressao = new RegExp(this.value, "i"); // parâmetro "i" = não faz distinção entre letras maiúsculas e minúsculas
 
           if (!expressao.test(nome)) {
               paciente.classList.add("invisivel");
